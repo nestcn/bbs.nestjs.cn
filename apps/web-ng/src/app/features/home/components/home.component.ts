c67c01f058843142ad2b18fbb6781ca38fb21adc
周刘通 <zltong>
2020-12-19 16:48:26 +0800

    #update drink some tea ~

--- apps/web-ng/src/app/features/home/components/home.component.ts
+++ apps/web-ng/src/app/features/home/components/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 import { BaseComponent } from '@myworkspace/core';
 
@@ -6,4 +6,24 @@ import { BaseComponent } from '@myworkspace/core';
   selector: 'nx-home',
   templateUrl: 'home.component.html',
 })
-export class HomeComponent extends BaseComponent {}
+export class HomeComponent extends BaseComponent implements OnInit {
+  /** 工具栏抽屉是否打开 */
+  drawerOpen: boolean = false;
+  /** 是否滚动过 */
+  scrolled: boolean = false;
+
+  constructor(
+  ) {
+    super()
+  }
+  ngOnInit(): void {
+  }
+
+
+  @HostListener('window:scroll', ['$event'])
+  windowsScroll(event: Event) {
+    if (window.pageYOffset) {
+      if (!this.scrolled) { this.scrolled = true; }
+    } else { this.scrolled = false; }
+  }
+}
